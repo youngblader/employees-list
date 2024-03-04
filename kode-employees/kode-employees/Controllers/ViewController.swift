417c763e7631b2364b089caf15a862fab7874a87
Eugene <56676493+youngblader@users.noreply.github.com>
2024-03-04 19:43:05 +0300

    feat: init app

---
+++ kode-employees/kode-employees/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  kode-employees
+//
+//  Created by Евгений Зорич on 4.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
