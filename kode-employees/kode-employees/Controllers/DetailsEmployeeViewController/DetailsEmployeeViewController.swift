093a6e8201143b6f4facb230c7e4f207ae817499
Eugene <56676493+youngblader@users.noreply.github.com>
2024-03-05 15:40:09 +0300

    chore: add comments, init property

--- kode-employees/kode-employees/Controllers/DetailsEmployeeViewController/DetailsEmployeeViewController.swift
+++ kode-employees/kode-employees/Controllers/DetailsEmployeeViewController/DetailsEmployeeViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 final class DetailsEmployeeViewController: UIViewController {
+    private let employee: Employee
+
+    //MARK: - Views
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //MARK: - Init
+    init(employee: Employee) {
+        self.employee = employee
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
