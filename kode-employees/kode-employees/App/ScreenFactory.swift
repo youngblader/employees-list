a483aaeb7269a8ecd2704e78bff9e369f0d40423
Eugene <56676493+youngblader@users.noreply.github.com>
2024-03-06 12:48:57 +0300

    refactor: refactoring app deps

--- kode-employees/kode-employees/App/ScreenFactory.swift
+++ kode-employees/kode-employees/App/ScreenFactory.swift
@@ -11,20 +11,20 @@ protocol ScreenFactory {
     var di: Di! { get set }
     
     func createEmployeesController() -> UINavigationController
-    func createDetailsEmployeeController() -> DetailsEmployeeViewController
+    func createDetailsEmployeeController(_ employee: Employee) -> DetailsEmployeeViewController
 }
 
 final class ScreenFactoryImpl: ScreenFactory {
     weak var di: Di!
     
     func createEmployeesController() -> UINavigationController {
-        let controller = EmployeesViewController()
+        let controller = EmployeesViewController(employeeProvider: di.employeesProvider)
 
         return UINavigationController(rootViewController: controller)
     }
     
-    func createDetailsEmployeeController() -> DetailsEmployeeViewController {
-        let controller = DetailsEmployeeViewController()
+    func createDetailsEmployeeController(_ employee: Employee) -> DetailsEmployeeViewController {
+        let controller = DetailsEmployeeViewController(employee: employee)
         
         return controller
     }
