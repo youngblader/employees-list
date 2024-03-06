818fd5e3cc2faf78b320ac84800c552c1a217a8d
Eugene <56676493+youngblader@users.noreply.github.com>
2024-03-06 12:49:18 +0300

    fix: fix model

--- kode-employees/kode-employees/Models/Employee.swift
+++ kode-employees/kode-employees/Models/Employee.swift
@@ -11,10 +11,58 @@ import Foundation
 struct Employee: Codable {
     let id: String
     let avatarUrl: String
-    let firstName, lastName, userTag, department: String
+    let firstName, lastName, userTag: String
     let position, birthday, phone: String
+    let department: Department
     
     func getEmployeeFullName() -> String {
         return "\(firstName) \(lastName)"
+    }
+}
+
+enum Department: String, CodingKey, Codable, CaseIterable {
+    case all
+    case android
+    case iOS = "ios"
+    case design
+    case management
+    case qa
+    case backOffice = "back_office"
+    case frontend
+    case hr
+    case pr
+    case backend
+    case support
+    case analytics
+
+    var title: String {
+        switch self {
+        case .all:
+            return "Все"
+        case .android:
+            return "Android"
+        case .iOS:
+            return "iOS"
+        case .design:
+            return "Design"
+        case .management:
+            return "Management"
+        case .qa:
+            return "QA"
+        case .backOffice:
+            return "BackOffice"
+        case .frontend:
+            return "Frontend"
+        case .hr:
+            return "HR"
+        case .pr:
+            return "PR"
+        case .backend:
+            return "Backend"
+        case .support:
+            return "Support"
+        case .analytics:
+            return "Analytics"
+        }
     }
 }
